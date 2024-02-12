627a5865803d4309d6eb8ece2082e8351c2b72f7
iqbalmuflih nwdt <muhammad.iqbalmuflihuddin@nawadata.id>
2024-02-12 09:10:27 +0700

    chapter 5 done

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,7 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter, Lusitana, Work_Sans } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 
 export const lusitana = Lusitana({ subsets: ['latin'], weight:['400', '700'] });
+
+export const worksans = Work_Sans({ subsets: ['latin'], weight:['400', '700'] });
